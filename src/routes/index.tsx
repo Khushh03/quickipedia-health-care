2468a5d78431a2cd837a792b60d4c6034d11bd84
khushbu <khushbu9625@gmail.com>
2026-01-22 22:05:53 +0530

    //

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,22 @@
-import { createFileRoute } from '@tanstack/react-router'
-import Navbar from './components--/navbar/navbar'
-import Footer from './components--/footer/footer'
-import HeroSection from './components--/sections/hero_section'
+import { createFileRoute } from "@tanstack/react-router"
+import Navbar from "./-components/navbar/navbar"
+import Footer from "./-components/footer/footer"
+import HeroSection from "./-components/sections/hero_section"
 
-export const Route = createFileRoute('/')({
-  component: RouteComponent,
+export const Route = createFileRoute("/")({
+    component: RouteComponent,
 })
 
 function RouteComponent() {
-  return (
-    <>
-      <Navbar />
-      <div className="flex flex-col">
-        <main>
-          <HeroSection />
-        </main>
-        <Footer />
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Navbar />
+            <div className="flex flex-col">
+                <main>
+                    <HeroSection />
+                </main>
+                <Footer />
+            </div>
+        </>
+    )
 }
